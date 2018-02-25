e02758255898687ac0f698d82d2062bc00e660f5
Abhishek Kumar <abhishek.srivastava16@ymail.com>
2018-02-25 18:23:01 +0530

    my min and max functions

---
+++ min_max.py
@@ -0,0 +1,16 @@
+def my_max(x):
+	n = x[0]
+	for i in x:
+		if n > i:
+			pass
+		else:
+			n = i
+	return n
+
+def my_min(x):
+	n = x[0]
+	for i in x:
+		if n < i:
+			pass
+		else: n = i
+	return n
