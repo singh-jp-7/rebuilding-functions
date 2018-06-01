01f8492b54e7be648711596c7bc382b28b31d902
Abhishek Kumar <32292664+code-stacker@users.noreply.github.com>
2018-06-01 23:35:09 +0530

    Delete sq_sum.py

--- sq_sum.py
+++
@@ -1,8 +0,0 @@
-# Function to return the sum of squares of all the positive integer smaller than a positive integer n.
-
-def sq_sum(n):
-	x = [i*i for i in range(1,n)]
-	return sum(x)
-
-
-print sq_sum(4)
